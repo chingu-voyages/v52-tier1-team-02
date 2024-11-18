cf6c9daa746767cbc4c875bf90b47af1468ebe15
HalaM5 <hala_mm_5@yahoo.com>
2024-11-18 21:51:10 +0200

    store hard code but I need to store data from form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+/* Create database for store user data */
+let requ = indexedDB.open('userDataStored',1);
+requ.onupgradeneeded = function(event){
+    let dataInstance = event.target.result;
+    /* create object store data with unique phone path */
+    let storeDat = dataInstance.createObjectStore('users',path)
+}
+
+requ.onsuccess = function(event){
+    let dataInstance = event.target.result;
+    //create functio to add user 
+    function addUser(user){
+        let transaction = dataInstance.transaction(['users'],'readwrite')
+        let objectStore = transaction.objectStore('users')
+        let requ = objectStore.add(user)//relate with phone
+
+        requ.onsuccess= function(){
+            console.log('user added',user)
+        }
+        requ.onerror = function(){
+            console.error('Error addding user',requ.error)
+        }
+    
+    }
+    console.log(addUser(storeData()))
+
+}
+
 /* create function store data of user for first time */
 function storeData(){
     /* store user's data  in variable  */
@@ -13,13 +41,18 @@ function storeData(){
     }
     console.log(user.name);
     console.log(user.phone);
-}
+    let senddata = user ;
+    console.log(senddata);
+    return senddata
 
+}
 let submitt = document.querySelector('#submit');
 submitt.addEventListener('click',(e)=>{
     e.preventDefault();
     storeData();
 })
+
+console.log(requ)
 
 
 
---
+++ js/database.js
@@ -0,0 +1,137 @@
+let u ;
+
+/* TODO: Check if indexedDb is avaliable on your browser*/
+const indexedDB = window.indexedDB ||
+window.mozIndexedDB ||
+window.webkitIndexedDB ||
+window.msIndexedDB ||
+window.shimIndexedDB;
+
+if  (!indexedDB){
+    console.log("Indexed is not on your browser");
+}
+else{
+    console.log('db is on your browser');
+    document.querySelector('.alert').innerHTML= 'db is on your browser'
+}
+/* TODO: create database in browser to store data from from */
+const request = indexedDB.open('visitRequest',1);
+
+/* TODO: inform us there is error during request */
+request.onerror = function (event){
+    console.error('error is occured');
+    console.error(event)
+};
+
+/* TODO: handle shape of data that will be store and how to work with in future */
+request.onupgradeneeded = function (){
+    const db = request.result;
+    const store = db.createObjectStore('peopleData', {keyPath: 'phone'});
+    store.createIndex('name_client',['name'],{unique: false});
+    store.createIndex('address_client',['address'],{unique: false});
+    store.createIndex('phone',['phone'],{unique: false});
+    store.createIndex('status',['status'],{unique: false});
+    store.createIndex('mail',['mail'],{unique: false});
+    store.createIndex('allData',['name','address','phone','mail','status'],{unique: false});
+}
+
+/* TODO: Add data and query to find it */
+request.onsuccess = function (){
+    console.log('database opened successfully');
+    
+    const db = request.result;
+
+    const transactionn = db.transaction('peopleData','readwrite');
+    const store = transactionn.objectStore('peopleData');
+    const statusIndex = store.index('status');
+    const phoneIndex = store.index('phone');
+    const mailIndex = store.index('mail');
+    const addressIndex = store.index('address_client');
+    const nameIndex = store.index('name_client');
+    const allDataIndex = store.index('allData')
+
+    //add data from input 
+    store.put({phone : 5656565,
+        name :'hh',
+        address : '23st cklklk',
+        status : 'pending',
+        mail: '@'
+    })
+    store.put({phone : 5555555,
+        name :'kkkk',
+        address : '55st 5ppppp',
+        status : 'pending',
+        mail: '@'
+})
+    store.put({phone : 2222255,
+        name :'mmmmmm',
+        address : '55st mmmp',
+        status : 'done',
+        mail: '@'
+})
+    
+
+    // store data fro form
+    let storeDataa = storeData();
+    if(storeDataa){
+        store.put(storeDataa)
+        console.log(storeDataa)
+    }  
+      
+    /** TODO: Query for data that stored */
+    const phoneQuery = store.get(5656565)// bring data for certain phone
+    const statusQuery = store.index('status').getAll(['done']);//return client with done status
+    const addressQuery = store.index('address_client').get(['23st cklklk'])// return user with specific address
+    const allDataQuery = store.index('allData').getAll()// bring all data for all user
+ // if query is succeeded
+    phoneQuery.onsuccess = function (){
+        console.log('phoneQuery of user ', phoneQuery.result);
+    }
+    statusQuery.onsuccess = function (){
+        console.log('status of user', statusQuery.result)
+    }
+    addressQuery.onsuccess = function (){
+        console.log('address of user', addressQuery.result)
+    }
+    allDataQuery.onsuccess = function (){
+        console.log('All data of user', allDataQuery.result)
+    }
+
+    transactionn.oncomplete = function (){
+        db.close()
+    }
+
+    
+}
+
+/* create function store data of user for first time */
+function storeData(){
+    /* store user's data  in variable  */
+    let namee =document.getElementById('name').value;
+    let phone = document.getElementById('phone').value;
+    let address = document.getElementById('address').value;
+    /*create object to store data user in it*/
+    if(namee && phone && address){
+        return {
+            name: namee,
+            phone: phone,
+            address: address,
+            status: 'pending',
+            mail: '@'
+        }
+    }else{
+        console.log('please fill in all field');
+        return 'null'
+    }
+
+}
+
+let submitt = document.querySelector('#submit');
+submitt.addEventListener('click',(e)=>{
+    e.preventDefault();
+    storeData();
+    console.log(storeData())
+    
+})
+
+ 
